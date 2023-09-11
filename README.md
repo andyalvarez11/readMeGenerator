ec74db44e03250cdeebae14d86c9df052daf0f15
andyalvarez11 <120332228+andyalvarez11@users.noreply.github.com>
2023-09-10 21:23:26 -0400

    updated js file

--- README.md
+++ README.md
@@ -1,16 +1,40 @@
-# readMeGenerator
-GIVEN a command-line application that accepts user input
-WHEN I am prompted for information about my application repository
-THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-WHEN I enter my project title
-THEN this is displayed as the title of the README
-WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-WHEN I choose a license for my application from a list of options
-THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-WHEN I enter my GitHub username
-THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-WHEN I enter my email address
-THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-WHEN I click on the links in the Table of Contents
-THEN I am taken to the corresponding section of the README
+
+# readmegenerator
+
+## Description
+by answering these prompts a readme file will be generated
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+To install dependencies, run the following command:
+```
+i dont know
+```
+
+## Usage
+im a begginer
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+please help
+
+## Tests
+To run tests, use the following command:
+```
+i dont know
+```
+
+## Questions
+If you have any questions, feel free to reach out to me on GitHub:
+[andyalvarez11](https://github.com/andyalvarez11)
+
+You can also contact me via email at alvarezandy11@yahoo.com.
--- index.js
+++ index.js
@@ -1,85 +1,117 @@
-const inquirer=require("inquirer");
-const fs=require("fs");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const generateReadMe = ({projectName, projectDescription, tableOfContent, installation, usage, contribution, tests, license}) =>
-`
-#${projectName}
-##${projectDescription}
-##${tableOfContent}
-##${installation}
-##${usage}
-##${contribution}
-##${tests}
-##${license}
+const generateReadMe = ({
+  projectName,
+  projectDescription,
+  installation,
+  usage,
+  userContribution,
+  commandTests,
+  license,
+  userName,
+  email,
+}) => {
+  return `
+# ${projectName}
 
+## Description
+${projectDescription}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+To install dependencies, run the following command:
+\`\`\`
+${installation}
+\`\`\`
+
+## Usage
+${usage}
+
+## License
+This project is licensed under the ${license} license.
+
+## Contributing
+${userContribution}
+
+## Tests
+To run tests, use the following command:
+\`\`\`
+${commandTests}
+\`\`\`
+
+## Questions
+If you have any questions, feel free to reach out to me on GitHub:
+[${userName}](https://github.com/${userName})
+
+You can also contact me via email at ${email}.
 `;
+};
 
-const project={
-    projectName:"",
-    projectDescription:"",
-    tableOfContent:"",
-    installation:"",
-    usage:"",
-    contribution:"",
-    tests:"",
-    license:""
-}
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "userName",
+      message: "What is your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+    },
+    {
+      type: "input",
+      name: "projectName",
+      message: "What is your project name?",
+    },
+    {
+      type: "input",
+      name: "projectDescription",
+      message: "Please write a short description of your project",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What kind of license should you have?",
+      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "NONE"],
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What command should be run to install dependencies?",
+    },
+    {
+      type: "input",
+      name: "commandTests",
+      message: "What command should be run to run tests?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What does the user need to know about using the repo?",
+    },
+    {
+      type: "input",
+      name: "userContribution",
+      message: "What does the user need to know about contributing to the repo?",
+    },
+  ])
+  .then((answers) => {
+    const readMeContent = generateReadMe(answers);
 
-inquirer.prompt([
-    {
-        type:"input",
-        name:"userName",
-        message:"What is your GitHub username?"
-    },
-    {
-        type:"input",
-        name:"email",
-        message:"What is your email address?"
-    },
-    {
-        type:"input",
-        name:"projectName",
-        message:"What is your project name?"
-    },
-    {
-        type:"input",
-        name:"projectDescription",
-        message:"Please write a short description of your project"
-    },
-    {
-        type:"list",
-        name:"license",
-        message:"What kind of license should you have?",
-        choices:["MIT","APACHE 2.0","GPL 3.0","BSD 3","NONE"]
-    },
-    {
-        type:"input",
-        name:"commandDependencies",
-        message:"What command should be run to install dependcies?"
-    },
-    {
-        type:"input",
-        name:"commandTests",
-        message:"What command should be run to run tests?"
-    },
-    {
-        type:"input",
-        name:"userKnow",
-        message:"What does the user need to know about using the repo?"
-    },
-    {
-        type:"input",
-        name:"userContribution",
-        message:"What does the user need to know about contributing to the repo?"
-    },
-]).then((answers) =>{
-    readMeContent = generateReadMe(answers);
-
-    fs.writeFile("README.md",readMeContent,(error)=>{
-        if(error){
-            console.log(error);
-        } else {
-            console.log("README generated successfully")
-        }
-    })
-});
+    fs.writeFile("README.md", readMeContent, (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("README generated successfully");
+      }
+    });
+  });
